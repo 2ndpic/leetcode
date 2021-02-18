ec068c5ebda425a710442a21db293330a87c07fa
zhoupengcheng <707480880@qq.com>
2021-02-18 15:47:40 +0800

    每日一题

---
+++ leetcode/editor/cn/[106]从中序与后序遍历序列构造二叉树.py
@@ -0,0 +1,32 @@
+# 根据一棵树的中序遍历与后序遍历构造二叉树。 
+# 
+#  注意: 
+# 你可以假设树中没有重复的元素。 
+# 
+#  例如，给出 
+# 
+#  中序遍历 inorder = [9,3,15,20,7]
+# 后序遍历 postorder = [9,15,7,20,3] 
+# 
+#  返回如下的二叉树： 
+# 
+#      3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+#  
+#  Related Topics 树 深度优先搜索 数组 
+#  👍 446 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+# leetcode submit region end(Prohibit modification and deletion)
