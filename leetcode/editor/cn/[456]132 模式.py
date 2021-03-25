58920a56109ca71a7b6fddd516e12f7957963807
zhoupengcheng <707480880@qq.com>
2021-03-25 19:33:38 +0800

    每日一题

--- leetcode/editor/cn/[456]132 模式.py
+++ leetcode/editor/cn/[456]132 模式.py
@@ -93,24 +93,37 @@ class Solution:
         """
         直觉告诉我这是一个单调栈解决的问题。
         先固定一个数
-        - 如果枚举i，需要找到jk对，所以需要从后往前遍历。
-            找jk对转换为找k，在[i,k]区间内存在大于k的j，要利用k知道j的存在，就需要维护一个单调栈
-            这个单调栈的性质在于k被更新，必须j比k大，所以维护一个单调递减栈（栈顶最小）
-            一旦k被更新了，就知道是有一个比k更大的数把一个数从单调栈pop出来了。
-            所以只用比较i和k的值即可，k比i大即存在j比i大 and j 必大于 k
+        - 如果枚举j，
+            由于 j 是 132 结构里最大的数，因此我们需要在 j 的右边中比 j 小的「最大」的数，
+            在 j 的左边找比 j 小的「最小」的数。
+            这很容易联想到单调栈，但是朴素的单调栈是帮助我们找到左边或者右边「最近」的数，无法直接满足我们「最大」和「最小」的要求，需要引入额外逻辑。
         """
-        stack = []
-        k = float('-inf')
-        for i in nums[::-1]:
-            if k > i:
-                return True
-            while stack and i > stack[-1]:
-                k = stack.pop()
-            stack.append(i)
+        mj = []     # mi 维护 nums[i] 左侧的最小值,mi是有单调减的（栈顶最小）
+        stack = []  # 维护 nums[i] 右侧比 mi[i] 大的值
+        for j in nums:
+            if not mj:
+                mj.append(j)
+            else:
+                mj.append(min(mj[-1], j))
+        for j in range(len(nums)-1, -1, -1):
+            if nums[j] > mj[j]:
+                """
+                现在nums[j]右边找一个比mi[j]大的元素
+                如果栈顶比mi[j]要小，之后肯定也不会用到了，丢掉（因为mi越往左走越大）
+                栈顶坐标>j,mj[j]坐标小于j，如果nums[j]的值比栈顶大，就直接返回了
+                """
+                while stack and stack[-1] <= mj[j]:
+                    stack.pop()
+                if stack and stack[-1] < nums[j]:
+                    return True
+                stack.append(nums[j])
         return False
+
+
+
 # leetcode submit region end(Prohibit modification and deletion)
-# nums = [1,2,3,4]
-nums = [3,1,4,2]
-nums = [-1,3,2,0]
-nums = [3,5,0,3,4] # True
+nums = [1,2,3,4]
+# nums = [3,1,4,2]
+# nums = [-1,3,2,0]
+# nums = [3,5,0,3,4] # True
 print(Solution().find132pattern(nums))
---
+++ leetcode/editor/cn/[84]柱状图中最大的矩形.py
@@ -0,0 +1,53 @@
+# 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。 
+# 
+#  求在该柱状图中，能够勾勒出来的矩形的最大面积。 
+# 
+#  
+# 
+#  
+# 
+#  以上是柱状图的示例，其中每个柱子的宽度为 1，给定的高度为 [2,1,5,6,2,3]。 
+# 
+#  
+# 
+#  
+# 
+#  图中阴影部分为所能勾勒出的最大矩形面积，其面积为 10 个单位。 
+# 
+#  
+# 
+#  示例: 
+# 
+#  输入: [2,1,5,6,2,3]
+# 输出: 10 
+#  Related Topics 栈 数组 
+#  👍 1263 👎 0
+
+from typing import List
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        """
+        1.此题的本质是找到每个柱形条左边和右边最近的比自己低的矩形条，然后用宽度乘上当前柱形条的高度就是以当前柱形条作为矩形高的矩形,作为备选答案。
+        2.解决此类问题的经典做法是单调栈，维护一个单调递增的栈，
+            如果当前柱形条i的高度比栈顶要低，则栈顶元素cur出栈。
+                出栈后，cur右边第一个比它低的柱形条就是i，左边第一个比它低的柱形条是当前栈中的top,pop出去计算得到的就是以cur作为矩形高的矩形面积
+            不断出栈直到栈为空或者柱形条i不再比top低。
+        3.满足2之后，当前矩形条i进栈
+        4.所有元素都要入栈出栈一次，所以时间复杂度O(n)
+        """
+        # 为了最后所有元素出栈方便，heights数组末尾添加高度-1
+        res = 0
+        heights.append(-1)  # 加了一个最小值，可以保证栈中所有元素出栈，这样每个元素作为高都计算得到一个矩形了
+        stack = []  # 单调递增栈,里面存的索引
+        for i in range(len(heights)):
+            while stack and heights[i] <= heights[stack[-1]]:
+                cur = heights[stack.pop()]
+                if stack:
+                    res = max(res, cur * (i - stack[-1] - 1))  # 为啥子还要-1,自己画图体会一下
+                else:
+                    res = max(res, cur * i)  # 此时cur前面没有元素了，自己画图体会下
+
+            stack.append(i)
+        return res
+# leetcode submit region end(Prohibit modification and deletion)
