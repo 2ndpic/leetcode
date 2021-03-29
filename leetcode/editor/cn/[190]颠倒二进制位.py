6b9fea7c8194445abbc28a7436422b97b633eb46
zhoupengcheng <707480880@qq.com>
2021-03-29 10:25:20 +0800

    每日一题

--- leetcode/editor/cn/[190]颠倒二进制位.py
+++ leetcode/editor/cn/[190]颠倒二进制位.py
@@ -66,8 +66,6 @@ class Solution:
             ans |= (n & 1) << (31 - i)
             n >>= 1
         return ans
-
-# leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def reverseBits(self, n: int) -> int:
         ans = 0
@@ -75,6 +73,16 @@ class Solution:
             ans = (ans << 1) + (n & 1)
             n >>= 1
         return ans
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        n = (n >> 16) | (n << 16)
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8)
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4)
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2)
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1)
+        return n
 # leetcode submit region end(Prohibit modification and deletion)
 n = 4294967293
 print(Solution().reverseBits(n))
