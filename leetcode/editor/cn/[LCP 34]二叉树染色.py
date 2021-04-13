285adf1c11399335a439587f0be33a1137233418
zhoupengcheng <707480880@qq.com>
2021-04-13 10:58:54 +0800

    每日一题

--- leetcode/editor/cn/[LCP 34]二叉树染色.py
+++ leetcode/editor/cn/[LCP 34]二叉树染色.py
@@ -26,8 +26,24 @@
 # + `1 <= val <= 10000`
 # + `1 <= 结点数量 <= 10000`
 #  👍 3 👎 0
+class Solution:
+    def maxValue(self, root: TreeNode, k: int) -> int:
+        """
+        超时了
+        """
+        def dfs(node, i):
+            ans = 0
+            if not node: return ans
+            if i == 0: return self.maxValue(node.left, k) + self.maxValue(node.right, k)
+            for j in range(i):
+                ans = max(ans, node.val + dfs(node.left, j) + dfs(node.right, i - j - 1))
+            return ans
 
-
+        ans = 0
+        if not root: return ans
+        for i in range(k + 1):
+            ans = max(ans, dfs(root, i))
+        return ans
 # leetcode submit region begin(Prohibit modification and deletion)
 # Definition for a binary tree node.
 # class TreeNode:
@@ -38,4 +54,31 @@
 
 class Solution:
     def maxValue(self, root: TreeNode, k: int) -> int:
+        """
+        递归
+        定义f[i]表示以该节点为根，相邻子节点染色的个数为i（包含自身）的情况下，节点价值总和的最大值; 0 <= i <= k
+        i == 0: 表示当前根节点为白色，无所谓相邻子节点的颜色，f[0] = max(fl) + max(fr)
+        i == 1: 表示当前根节点染色，且相邻子节点（左右孩子）都没有染色，f[1] = node.val + fl[0] + fr[0]
+        i == 2: 表示当前根节点染色，且相邻子节点左右孩子有一个染色,f[2] = node.val + max(fl[1] + fr[0], fl[1] + fr[0])
+        f[0] = max(fl) + max(fr) i = 0
+        f[i] = node.val + max(fl[j] + fr[i - j - 1]) 0 < i <= k and 0 <= j < i
+        初始状态：当前节点为空，f[i] = 0 (0 <= i <= k)
+        最终结果：根节点所有情况的最大值，max(f)
+        """
+        def dfs(root):
+            f = [0] * (k + 1)
+            if not root: return f
+            fl, fr = dfs(root.left), dfs(root.right)
+            f[0] = max(fl) + max(fr)
+            for i in range(1, k + 1):
+                for j in range(i):
+                    f[i] = max(f[i], root.val + fl[j] + fr[i - j - 1])
+            return f
+        return max(dfs(root))
 # leetcode submit region end(Prohibit modification and deletion)
+# [5,2,3,4]
+root = TreeNode(5)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+print(Solution().maxValue(root, 2))
