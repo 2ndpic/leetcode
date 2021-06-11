3a178b061d34016dba8fdb6be91691d285a92dd5
zhoupengcheng <707480880@qq.com>
2021-06-11 10:41:53 +0800

    每日一题

--- leetcode/editor/cn/[322]零钱兑换.py
+++ leetcode/editor/cn/[322]零钱兑换.py
@@ -51,8 +51,25 @@
 #  Related Topics 动态规划 
 #  👍 1309 👎 0
 
-
+from typing import List
+import functools
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        """
+        f[i][j]表示用前i种硬币凑到j的最少个数，转化为完全背包问题.f[n][amount]为答案
+        f[i][j] = min(f[i-1][j], f[i-1][j-coins[i-1]] + 1, f[i-1][j-2*coins[i-1]] + 2,...)
+        f[i][j-conis[i]] = min(  f[i-1][j-coins[i-1]], f[i-1][j-2*coins[i-1]] + 1,...)
+        so f[i][j] = min(f[i-1][j], f[i][j-coins[i-1]] + 1)
+        """
+        n = len(coins)
+        f = [float('inf')] * (amount + 1)
+        f[0] = 0
+        for i in range(1, n + 1):
+            for j in range(coins[i-1], amount + 1):
+                f[j] = min(f[j], f[j - coins[i-1]] + 1)
+        return f[-1] if f[-1] < float('inf') else -1
 # leetcode submit region end(Prohibit modification and deletion)
+coins = [1, 2, 5]; amount = 11
+coins = [1]; amount = 0
+print(Solution().coinChange(coins, amount))
