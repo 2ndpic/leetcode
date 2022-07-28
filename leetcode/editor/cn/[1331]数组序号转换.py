aeefeccd9d1114d37231ec8a3cb1ed05442f48aa
zpc's mac <707480880@qq.com>
2022-07-28 20:23:18 +0800

    每日一题

--- leetcode/editor/cn/[1331]数组序号转换.py
+++ leetcode/editor/cn/[1331]数组序号转换.py
@@ -10,14 +10,16 @@ class Solution:
         #         memo[v] = index
         #         index += 1
         # return [memo[v] for v in arr]
-        arr_sort = sorted([(i, v) for i, v in enumerate(arr)], key=lambda x: x[1])
-        ans = [0] * len(arr)
-        count, pre_v = 0, 0
-        for i, v in arr_sort:
-            if i == 0 or v != pre_v:
-                count += 1
-                pre_v = v
-            ans[i] = count
-        return ans
+        # arr_sort = sorted([(i, v) for i, v in enumerate(arr)], key=lambda x: x[1])
+        # ans = [0] * len(arr)
+        # count, pre_v = 0, 0
+        # for i, v in arr_sort:
+        #     if i == 0 or v != pre_v:
+        #         count += 1
+        #         pre_v = v
+        #     ans[i] = count
+        # return ans
+        memo = {v: i for i, v in enumerate(sorted(set(arr)), 1)}
+        return [memo[v] for v in arr]
 # leetcode submit region end(Prohibit modification and deletion)
 print(Solution().arrayRankTransform([37,12,28,9,100,56,80,5,12]))
