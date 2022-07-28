9c11e6ebc34e853184c9df74e2b2224a25b997e9
zpc's mac <707480880@qq.com>
2022-07-28 17:30:30 +0800

    每日一题

---
+++ leetcode/editor/cn/[1331]数组序号转换.py
@@ -0,0 +1,23 @@
+from typing import List
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        # arr_sort = sorted(arr)
+        # memo = {}
+        # index = 1
+        # for v in arr_sort:
+        #     if v not in memo:
+        #         memo[v] = index
+        #         index += 1
+        # return [memo[v] for v in arr]
+        arr_sort = sorted([(i, v) for i, v in enumerate(arr)], key=lambda x: x[1])
+        ans = [0] * len(arr)
+        count, pre_v = 0, 0
+        for i, v in arr_sort:
+            if i == 0 or v != pre_v:
+                count += 1
+                pre_v = v
+            ans[i] = count
+        return ans
+# leetcode submit region end(Prohibit modification and deletion)
+print(Solution().arrayRankTransform([37,12,28,9,100,56,80,5,12]))
