f27a9149c58af65b00b0803fc0031b6d4955ef8d
zpc's mac <707480880@qq.com>
2022-08-01 15:51:12 +0800

    easy

--- leetcode/editor/cn/[2357]使数组中所有元素都等于零.py
+++ leetcode/editor/cn/[2357]使数组中所有元素都等于零.py
@@ -2,5 +2,7 @@ from typing import List
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
+        nums = set(nums)
+        return len(nums) - (0 in nums)
 
 # leetcode submit region end(Prohibit modification and deletion)
